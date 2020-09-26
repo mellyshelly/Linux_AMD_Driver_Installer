536a25cde6492b752aa31dcc385d11d95d24c5f3
WaterTim <45728713+watertim@users.noreply.github.com>
2020-09-26 16:16:07 +0800

    Reformat

--- amd_install.sh
+++ amd_install.sh
@@ -14,79 +14,79 @@ cd /hive-drivers-pack/
 echo "Please note Drivers with the 18.04 suffix require an OS upgrade, or On Hiveos u can download the latest beta from http://download.hiveos.farm/ and install on a fresh usb."
 PS3='Please enter your choice Drivers: '
 
-options=("16.40-348864-ubuntu-16.04" "16.60-379184-ubuntu-16.04" "17.10-429170-ubuntu-16.04" "17.10-450821-ubuntu-16.04" "17.30-458935-ubuntu-16.04" "17.30-465504-ubuntu-16.04" "17.40-492261-ubuntu-16.04" "17.40-483984-ubuntu-16.04" "17.50-511655-ubuntu-16.04" "18.10-572953-ubuntu-16.04" "18.20-606296-ubuntu-18.04" "18.30-641594-ubuntu-18.04" "18.30-641594-ubuntu-16.04" "18.40-673869-ubuntu-16.04" "18.40-697810-ubuntu-18.04" "18.50-725072-ubuntu-18.04" "19.10-785425-ubuntu-18.04" "19.20-812932-ubuntu-18.04" "19.30-838629-ubuntu-18.04" "19.50-967956-ubuntu-18.04" "Quit")
+options=("16.40-348864-ubuntu-16.04" "16.60-379184-ubuntu-16.04" "17.10-429170-ubuntu-16.04" "17.10-450821-ubuntu-16.04" "17.30-458935-ubuntu-16.04" "17.30-465504-ubuntu-16.04" "17.40-492261-ubuntu-16.04" "17.40-483984-ubuntu-16.04" "17.50-511655-ubuntu-16.04" "18.10-572953-ubuntu-16.04" "18.20-606296-ubuntu-18.04" "18.30-641594-ubuntu-18.04" "18.30-641594-ubuntu-16.04" "18.40-673869-ubuntu-16.04" "18.40-697810-ubuntu-18.04" "18.50-725072-ubuntu-18.04" "19.10-785425-ubuntu-18.04" "19.20-812932-ubuntu-18.04" "19.30-838629-ubuntu-18.04" "19.50-967956-ubuntu-18.04" "20.30-1109583-ubuntu-18.04" "Quit")
 
 select opt in "${options[@]}"
 do
 case $opt in
-    "16.40-348864-ubuntu-16.04")
+    	"16.40-348864-ubuntu-16.04")
     wget http://download.hiveos.farm/drivers/amdgpu-pro-16.40-348864.tar.xz
         version="16.40-348864";break
     ;;
-    "16.60-379184-ubuntu-16.04")
+    	"16.60-379184-ubuntu-16.04")
 	wget http://download.hiveos.farm/drivers/amdgpu-pro-16.60-379184.tar.xz
         version="16.60-379184";break
     ;;
-    "17.10-429170-ubuntu-16.04")
+    	"17.10-429170-ubuntu-16.04")
     wget http://download.hiveos.farm/drivers/amdgpu-pro-17.10-429170.tar.xz
         version="17.10-429170";break
     ;;
-    "17.10-450821-ubuntu-16.04")
+    	"17.10-450821-ubuntu-16.04")
     wget http://download.hiveos.farm/drivers/amdgpu-pro-17.10-450821.tar.xz
         version="17.10-450821";break
     ;;
-    "17.30-458935-ubuntu-16.04")
+    	"17.30-458935-ubuntu-16.04")
 	wget http://download.hiveos.farm/drivers/amdgpu-pro-17.30-458935.tar.xz
         version="17.30-458935";break
     ;;
-    "17.30-465504-ubuntu-16.04")
+    	"17.30-465504-ubuntu-16.04")
 	wget http://download.hiveos.farm/drivers/amdgpu-pro-17.30-465504.tar.xz
         version="17.30-465504";break
     ;;
-    "17.40-492261-ubuntu-16.04")
+    	"17.40-492261-ubuntu-16.04")
 	wget http://download.hiveos.farm/drivers/amdgpu-pro-17.40-492261.tar.xz
         version="17.40-492261";break
     ;; 
-    "17.40-483984-ubuntu-16.04")
+    	"17.40-483984-ubuntu-16.04")
 	wget http://download.hiveos.farm/drivers/amdgpu-pro-17.40-483984.tar.xz
         version="17.40-483984";break
     ;;
-    "17.50-511655-ubuntu-16.04")
+    	"17.50-511655-ubuntu-16.04")
 	wget http://download.hiveos.farm/drivers/amdgpu-pro-17.50-511655.tar.xz
         version="17.50-511655";break
     ;;
-    "18.10-572953-ubuntu-16.04")
+    	"18.10-572953-ubuntu-16.04")
 	wget http://download.hiveos.farm/drivers/amdgpu-pro-18.10-572953.tar.xz
         version="18.10-572953";break
     ;;
-    "18.20-606296-ubuntu-18.04")
+    	"18.20-606296-ubuntu-18.04")
 	wget http://download.hiveos.farm/drivers/amdgpu-pro-18.20-606296-18.04.tar.xz
         version="18.20-606296-18.04";break
     ;;
-    "18.30-641594-ubuntu-18.04")
+    	"18.30-641594-ubuntu-18.04")
 	wget http://download.hiveos.farm/drivers/amdgpu-pro-18.30-641594-18.04.tar.xz
         version="18.30-641594-18.04";break
     ;;
-    "18.30-641594-ubuntu-16.04")
+    	"18.30-641594-ubuntu-16.04")
 	wget http://download.hiveos.farm/drivers/amdgpu-pro-18.30-641594.tar.xz
         version="18.30-641594";break
     ;;
-    "18.40-673869-ubuntu-16.04")
+    	"18.40-673869-ubuntu-16.04")
 	wget http://download.hiveos.farm/drivers/amdgpu-pro-18.40-673869-ubuntu-16.04.tar.xz
         version="18.40-673869-ubuntu-16.04";break
     ;;
-    "18.40-697810-ubuntu-18.04")
+    	"18.40-697810-ubuntu-18.04")
 	wget http://download.hiveos.farm/drivers/amdgpu-pro-18.40-697810-ubuntu-18.04.tar.xz
         version="18.40-697810-ubuntu-18.04";break
     ;;
-    "19.10-785425-ubuntu-18.04")
+    	"19.10-785425-ubuntu-18.04")
 	wget http://download.hiveos.farm/drivers/amdgpu-pro-19.10-785425-ubuntu-18.04.tar.xz
         version="19.10-785425-ubuntu-18.04";break
     ;;
 	"19.10-725072-ubuntu-18.04")
 	wget http://download.hiveos.farm/drivers/amdgpu-pro-19.10-785425-ubuntu-18.04.tar.xz
         version="19.10-725072-ubuntu-18.04";break
-	;;
+    ;;
 	"19.20-812932-ubuntu-18.04")
 	wget http://download.hiveos.farm/drivers/amdgpu-pro-19.20-812932-ubuntu-18.04.tar.xz
         version="19.20-812932-ubuntu-18.04";break	
@@ -102,7 +102,7 @@ case $opt in
 	"20.30-1109583-ubuntu-18.04")
 	wget https://drivers.amd.com/drivers/linux/amdgpu-pro-20.30-1109583-ubuntu-18.04.tar.xz
         version="20.30-1109583-ubuntu-18.04";break
-	;;
+    ;;
 	"Quit")
 	exit
         break
